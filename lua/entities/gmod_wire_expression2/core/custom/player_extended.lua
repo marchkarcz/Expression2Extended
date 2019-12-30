fc99fa560bdaa5eb70e57d4b73502f5d7602b522
March Cain <marchcain1@gmail.com>
2019-12-29 23:47:40 -0800

    slayPlayer() and slay if damage below or equal to 0

--- lua/entities/gmod_wire_expression2/core/custom/player_extended.lua
+++ lua/entities/gmod_wire_expression2/core/custom/player_extended.lua
@@ -20,6 +20,24 @@ e2function void entity:doDamage(number damage,number damageType)
         Damage:SetInflictor(this)
         Damage:SetDamageType(damageType)
 		this:TakeDamageInfo(Damage)
+        if(this:Health() <= 0){
+            this:Kill()
+        }
+	end		
+end
+
+e2function void entity:slayPlayer()
+	local isAdmin = false
+	local propProtection = false
+	if this:IsValid() then 
+		if game.SinglePlayer() then isAdmin = true end
+		if self.player:IsAdmin() then isAdmin = true end
+		if !isAdmin then
+			propProtection = ( this == self.player || E2Lib.isOwner(self, this) || E2Lib.isFriend( this, self.player) )
+		end
+	end	
+	if (( isAdmin || propProtection ) && this:IsPlayer()) then
+        this:Kill()
 	end		
 end
 
---
+++ update.bat
@@ -0,0 +1,2 @@
+D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\gmad.exe create -folder C:\Users\march\Documents\1E2E -out D:\SteamLibrary\steamapps\common\GarrysMod\bin\expression2_extended.gma
+D:\SteamLibrary\steamapps\common\GarrysMod\bin\gmpublish.exe update -id 1952409500 -addon D:\SteamLibrary\steamapps\common\GarrysMod\bin\expression2_extended.gma -icon D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\Expression2Extended\expression2extended.jpg 
