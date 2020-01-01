48f7becd1541bac1682f0f1722db1c165cdef732
March Cain <marchcain1@gmail.com>
2019-12-31 23:07:21 -0800

    Mis-spell

--- lua/entities/gmod_wire_expression2/core/custom/player_extended.lua
+++ lua/entities/gmod_wire_expression2/core/custom/player_extended.lua
@@ -1,32 +1,35 @@
-__e2setcost(50)
+__e2setcost(1)
 
 E2Lib.RegisterExtension( "Player Extended", true, "Adds in more commands to run on players.")
 util.AddNetworkString("consolemessage")
 
 e2function void entity:doDamage(number damage,number damageType)
 	local isAdmin = false
+    local thisentity = this
 	local propProtection = false
 	if this:IsValid() then 
-		if game.SinglePlayer() then isAdmin = true end
 		if self.player:IsAdmin() then isAdmin = true end
 		if !isAdmin then
-			propProtection = ( this == self.player || E2Lib.isOwner(self, this) || E2Lib.isFriend( this, self.player) )
+			propProtection = ( thisentity == self.player || E2Lib.isOwner(self, thisentity) || E2Lib.isFriend( thisentity, self.player) )
 		end
 	end	
-	if (( isAdmin || propProtection ) && this:IsPlayer()) then
-        local Damage = DamageInfo()
+    local Damage = DamageInfo()
+	if (( isAdmin || propProtection ) && thisentity:IsPlayer()) then
         Damage:SetDamage(damage)
         Damage:SetAttacker(self.player)
         Damage:SetDamageType(damageType)
-		this:TakeDamageInfo(Damage)
+		thisentity:TakeDamageInfo(Damage)
         
         net.Start("consolemessage")
-        net.WriteString(self.player:Name() .. " did " .. damage .. " on you!")
-        net.Send(this)            
-        if this:Health() <= 0 then
-            this:Kill()
+        net.WriteString(self.player:Name() .. " did " .. damage .. " damage on you!")
+        net.Send(thisentity)            
+        if thisentity:Health() <= 0 then
+            if thisentity:Alive() then
+                thisentity:Kill()
+            end
         end
     end
+
 end
 
 e2function void entity:slayPlayer()
@@ -40,7 +43,12 @@ e2function void entity:slayPlayer()
 		end
 	end	
 	if (( isAdmin || propProtection ) && this:IsPlayer()) then
-        this:Kill()
+        if this:Alive() then
+			net.Start("consolemessage")
+        	net.WriteString(self.player:Name() .. " killed you!")
+        	net.Send(this)
+            this:Kill()
+        end
 	end		
 end
 
@@ -56,6 +64,9 @@ e2function void entity:setPlayerFOV(number fov, number time)
 		end
 	end	
 	if (( isAdmin || propProtection ) && this:IsPlayer()) then
+			net.Start("consolemessage")
+        	net.WriteString(self.player:Name() .. " set your FOV to " .. fov .. "!")
+        	net.Send(this)
         this:SetFOV(fov, time)
 	end		
 end
