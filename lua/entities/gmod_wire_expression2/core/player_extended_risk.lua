2b8bf6fc8644074413a56e58593c4b2f06f93c7f
March Cain <marchcain1@gmail.com>
2020-01-04 22:00:55 -0800

    Create player_extended_risk.lua

---
+++ lua/entities/gmod_wire_expression2/core/player_extended_risk.lua
@@ -0,0 +1,10 @@
+E2Lib.RegisterExtension( "Player Extended Risk", false, "Adds in more (semi-abusable) commands to run on players.", "This is disabled by default; I highly recommend keeping this disabled unless you really trust your staff." )
+
+
+e2function void entity:setWeaponColor(vector rgb) 
+	if self.player:isAdmin() then
+        
+    else
+        -- they can't run this function
+    end
+end
